1d33aaaa33235efdca746d44c06434571b4ff66d
nitzan <nitzancch@gmail.com>
2018-12-26 17:27:16 +0200

    TPC

---
+++ src/main/java/bgu/spl/net/srv/ThreadsPerClient.java
@@ -0,0 +1,17 @@
+package bgu.spl.net.srv;
+
+import bgu.spl.net.api.MessageEncoderDecoder;
+import bgu.spl.net.api.MessagingProtocol;
+
+import java.util.function.Supplier;
+
+public class ThreadsPerClient<T> extends BaseServer<T> {
+    public ThreadsPerClient(int port, Supplier<MessagingProtocol<T>> protocolFactory, Supplier<MessageEncoderDecoder<T>> encdecFactory) {
+        super(port, protocolFactory, encdecFactory);
+    }
+
+    @Override
+    protected void execute(BlockingConnectionHandler<T> handler) {
+        new Thread(handler).start();
+    }
+}
