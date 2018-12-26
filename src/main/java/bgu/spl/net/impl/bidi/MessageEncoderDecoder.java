bfbe084275d003739dfd4cbc387c2543e22d3a04
nitzan <nitzancch@gmail.com>
2018-12-26 17:32:59 +0200

    CHangeDECENC

--- src/main/java/bgu/spl/net/impl/bidi/MessageEncoderDecoder.java
+++ src/main/java/bgu/spl/net/impl/bidi/MessageEncoderDecoder.java
@@ -13,4 +13,19 @@ public class MessageEncoderDecoder implements bgu.spl.net.api.MessageEncoderDeco
     public byte[] encode(Object message) {
         return new byte[0];
     }
+
+    public short bytesToShort(byte[] byteArr)
+    {
+        short result = (short)((byteArr[0] & 0xff) << 8);
+        result += (short)(byteArr[1] & 0xff);
+        return result;
+    }
+
+    public byte[] shortToBytes(short num)
+    {
+        byte[] bytesArr = new byte[2];
+        bytesArr[0] = (byte)((num >> 8) & 0xFF);
+        bytesArr[1] = (byte)(num & 0xFF);
+        return bytesArr;
+    }
 }
